f9c25dcec5b0a28a15ccd39531a9e97ed6450bb5
petedaogit <143330101+petedaogit@users.noreply.github.com>
2024-11-06 18:54:34 +0700

    Update PlayerContext.jsx

--- SpotifyClone/src/context/PlayerContext.jsx
+++ SpotifyClone/src/context/PlayerContext.jsx
@@ -7,7 +7,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://web83-finalproject-backend.onrender.com";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
